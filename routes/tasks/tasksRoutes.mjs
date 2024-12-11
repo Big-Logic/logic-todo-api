c0a1d67427fd020e79db06219f0c431c157d8a3b
Big-Logic <alfredngwayah16@gmail.com>
2024-12-11 09:28:22 +0000

    implements deleting a task and the route for deleting a task

--- routes/tasks/tasksRoutes.mjs
+++ routes/tasks/tasksRoutes.mjs
@@ -3,12 +3,13 @@ import express from "express";
 import createTasks from "../../services/tasks/createTasks.mjs";
 import getTasks from "../../services/tasks/getTasks.mjs";
 import updateTasks from "../../services/tasks/updateTasks.mjs";
+import deleteTasks from "../../services/tasks/deleteTasks.mjs";
 
 const router = express.Router();
 
 // root endpoint: /api/v1/tasks
 
-router.route('/:id').patch(updateTasks.updateOne);
+router.route('/:id').patch(updateTasks.updateOne).delete(deleteTasks.deleteOne);
 
 router.route('/').post(createTasks.createOne).get(getTasks.getBulk);
 
--- services/tasks/deleteTasks.mjs
+++ services/tasks/deleteTasks.mjs
@@ -1,5 +1,31 @@
-const deleteOne = async (req, res) => {};
+// db
+import supabase from "../../config/supabase.config.mjs";
 
-const deleteBulk = async (req, res) => {};
+// utils
+import asyncWrapper from "../../utils/asyncWrapper.mjs";
+import SupabaseError from "../../utils/supabaseError.mjs";
+
+const deleteOne = asyncWrapper(async (req, res) => {
+  // req params
+  const { params } = req;
+//   params.user_id = "778c7688-69da-4fa8-bf11-2864db91815f";
+
+  // populate db
+  const { error } = await supabase
+    .from("tasks")
+    .delete()
+    .eq("id", params.id)
+    .eq("user_id", params.user_id);
+
+
+  // throw supabase error to global error handler
+  if (error) {
+    throw new SupabaseError(error);
+  }
+
+  res.status(204).json();
+});
+
+const deleteBulk = asyncWrapper(async (req, res) => {});
 
 export default { deleteOne, deleteBulk };
