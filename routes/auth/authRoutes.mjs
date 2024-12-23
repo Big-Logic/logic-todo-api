28843a3670afc338ddebafc0210f9996ecbd9073
Big-Logic <alfredngwayah16@gmail.com>
2024-12-23 01:18:46 +0000

    created email verification service after signup

--- routes/auth/authRoutes.mjs
+++ routes/auth/authRoutes.mjs
@@ -3,10 +3,13 @@ import express from "express";
 // Handlers
 import signup from "../../services/auth/signup.mjs";
 import login from "../../services/auth/login.mjs";
+import verifyEmail from "../../services/notification/verifyEmail.mjs";
 
 const router = express.Router();
 
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.get('/verify-email', verifyEmail);
+
 export default router;
